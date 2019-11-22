ef3fcd080aee10f5cc85bc6cef4f1b70c0499d67
threethan <ethanjmedei@gmail.com>
2019-11-22 04:16:49 -0500

    General beautification.  - Removed unused code  - Rearranged and cleaned up js  - Improved documentation on index.js  - Removed custom scroll handling  + Fixed a bug where page height would not correctly update upon resize

--- recources/scripts/index.js
+++ recources/scripts/index.js
@@ -1,20 +1,19 @@
-// JavaScript Document
 var defaultPage = 0;
 var prevPage, prevPrevPage;
 
 const PAGE_NAMES = ['designer', 'developer', 'mentor'];
 
+// Check for legacy versions of Internet Explorer
 if (navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1)) {
 	alert("Internet Explorer is not supported by this website. Consider using a newer browser such as Mozilla Firefox. Trust me, you won't regret it.");
 }
 
+// Setup
 $(document).ready( function() {
-	switchPage(defaultPage);
-	//updateParallax();
-  $('#nav-menu-text').html(PAGE_NAMES[defaultPage]);
+	switchPage(defaultPage); //Display the default page
+  $('#nav-menu-text').html(PAGE_NAMES[defaultPage]); //Update the dropdown indicator with the current page name
 
-  updateSbWidth();
-
+	// Interactable image setup
   $('img.pop').click( function() {
     $('#popup-img-img').prop('src', $(this).prop('src') );
     $('#popup-img-back').removeClass('no-img');
@@ -24,43 +23,7 @@ $(document).ready( function() {
   });
 });
 
-$(window).resize( function() {
-  updateSbWidth();
-});
-function updateSbWidth() {
-	outerWidthTemp = $('.no-scrollbar-parent').first().outerWidth();
-	innerWidthTemp = $('#sb-test-inner').first().width();
-	$(':root').css('--sb-width', innerWidthTemp - outerWidthTemp + 'px');
-
-}
-
-//[OLD] Called whenever an iframe opens a new page
-/* function iframeLoad(elem) {
-	try {
-		e = document.getElementById('iframe'+prevPage)
-		if (e) { //Ingore it running on a null element on initial load
-			//Will fail if url is from another site
-			x = e.contentWindow.location.href
-			|| e.contentDocument.location.href;
-		}
-	}
-	catch {
-	 	$('#top-bar-normal').addClass('hidden');
-		$('#top-bar-return').removeClass('hidden');
-	}
-}
-function goBack(redir=true) {
-	if (redir) document.getElementById('iframe'+prevPage).src = $('#iframe'+prevPage).data('src');
-	$('#top-bar-normal').removeClass('hidden');
-	$('#top-bar-return').addClass('hidden');
-	ignoreIframeChanges = true;
-} */
-
-$(document).on('click', function() {
-	hidePopups();
-});
-
-
+// Opens the page selection dropdown
 function showDd() {
 	nddTemp = $('#nav-dd')
 	$(nddTemp).removeClass('hidden');
@@ -69,13 +32,22 @@ function showDd() {
 	$(nddTemp).css("left", posTemp.left);
 }
 
+// Hides all pop-up menus. (Currently only the page selection dropdown)
 function hidePopups() {
 	$('#nav-dd').addClass('hidden');
 }
+$(document).on('click', function() { hidePopups(); }); //Calls hidePopups when any element is clicked
 
+// Updates the height of the content wrapper, called on page change or window size change. (Defines the vertical scroll limit)
+function updateContentHeight() {
+		$('#content-wrapper').css('height', $('#page'+prevPage).height());
+}
+$(window).resize( function() { updateContentHeight(); }); //Calls updateContentHeight when window is resized
+
+// Switches the currently displayed page. (Called by clicking the page select dropdown)
 var isBefore;
 function switchPage(num) {
-	//Ignore if trying to switch to the page we're already on or switching to
+	// Ignore if trying to switch to the page we're already on or switching to
 	if (num == prevPage) return;
 
 	lower = Math.min(prevPage, num);
@@ -112,10 +84,10 @@ function switchPage(num) {
 	prevPrevPage = prevPage;
 	prevPage = num;
 
-	$('#content-wrapper').css('height', $('#page'+num).height());
-
+	updateContentHeight();
 	setTimeout(switchPageDeferred, 1);
 }
+// Called 1ms after switchPage, after changes are applied to the DOM
 var animTimeout;
 function switchPageDeferred() {
 	e = $('#page'+prevPrevPage);
@@ -125,6 +97,7 @@ function switchPageDeferred() {
 	animTimeout = setTimeout(switchPageDone, 1000);
 	$('#page'+prevPage).css('transform','');
 }
+// Called upon completion of the page swith animation
 function switchPageDone() {
 	e = $('#page'+prevPrevPage);
 	$(e).addClass('hidden');
---
+++ recources/scripts/scrolling.js
@@ -0,0 +1,10 @@
+// Finds the width of the current browser's scroll bar and updates CSS to hide it
+function updateSb() {
+	outerWidthTemp = $('.no-scrollbar-parent').first().outerWidth();
+	innerWidthTemp = $('#sb-test-inner').first().width();
+	$(':root').css('--sb-width', innerWidthTemp - outerWidthTemp + 'px');
+}
+
+// Calls updateSb when needed
+$(document).ready( function() { updateSb(); }); //On page load
+$(window).resize( function()  { updateSb(); }); //On resize
